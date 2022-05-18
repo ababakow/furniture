90eace71c4420f33d06a9b7ccb90188591873488
Andrii Babakov <ababakow@gmail.com>
2022-05-18 18:38:58 +0300

    basic layouts

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const ejsMate = require('ejs-mate');
+
+const path = require('path');
 ////////////////////////////////////////////////////
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+//-------------------------------------------------
+app.use(express.static(path.join(__dirname, 'public')));
 ////////////////////////////////////////////////////
 
 app.get('/', (req, res) => {
