c29d1f3dc0d9218306fe91cc6ab7abc85e0ca215
Andrii Babakov <ababakow@gmail.com>
2022-05-24 12:44:51 +0300

    + basic contact form

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const ejsMate = require('ejs-mate');
+const mongoose = require('mongoose');
 
 const path = require('path');
 ////////////////////////////////////////////////////
+const dbUrl = 'mongodb://localhost:27017/furniture';
+mongoose
+	.connect(dbUrl)
+	.then(() => {
+		console.log('DB connected!');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+//-------------------------------------------------
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
