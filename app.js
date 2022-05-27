62c2444d8bc4ef43c035d465fceb1c601522c836
Andrii Babakov <ababakow@gmail.com>
2022-05-27 21:30:48 +0300

    add routes (+fixed mistake)

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const express = require('express');
 const app = express();
 const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
-const mailer = require('./nodemailer/nodemailer');
 
 const catalogRoutes = require('./routes/catalog');
 const shopRoutes = require('./routes/shop');
